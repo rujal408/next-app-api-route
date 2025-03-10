9a9af93cdf5dd1ca96777b78c3e67dccf1f86e71
rujal408 <rujal408@gmail.com>
2025-03-10 09:04:41 +0545

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+export type NextMiddleware = () => Promise<NextResponse>;
+
+type Middleware = (
+  req: NextRequest,
+  res: NextResponse,
+  next: NextMiddleware
+) => Promise<NextResponse>;
+
+class ApiRoute {
+  //  RECURSIVE TECHNIQUE IS BEING USED
+  use(...funcs: Middleware[]) {
+    return async (req: NextRequest, res: NextResponse) => {
+      const execute = async (index: number): Promise<NextResponse> => {
+        if (index >= funcs.length) {
+          return NextResponse.json(
+            { error: "No handler found" },
+            { status: 404 }
+          );
+        }
+
+        const currentFunc = funcs[index];
+        return currentFunc(req, res, () => execute(index + 1));
+      };
+
+      return execute(0);
+    };
+  }
+}
+
+export default ApiRoute;
