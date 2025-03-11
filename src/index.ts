468a5e7bb8ec481268d19e04bc3c8487600e9fd3
rujal408 <rujal408@gmail.com>
2025-03-11 16:57:54 +0545

    document code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * Type definition for the next middleware function in the chain
+ * @param data - Optional data to pass to the next middleware
+ * @returns Promise resolving to a NextResponse
+ */
 export type NextMiddleware = (data?: any) => Promise<NextResponse>;
 
-type Middleware = (
+/**
+ * Request function type definition
+ * @param req - Next.js request object
+ * @param res - Next.js response object
+ * @param next - Function to call the next middleware in the chain
+ * @param data - Optional data received from previous middleware
+ * @returns Promise resolving to a NextResponse
+ */
+type RequestChain = (
   req: NextRequest,
   res: NextResponse,
   next: NextMiddleware,
   data?: any
 ) => Promise<NextResponse>;
 
+/**
+ * ApiRoute class for creating request chains with data passing capabilities
+ */
 class ApiRoute {
-  use(...funcs: Middleware[]) {
+  use(...funcs: RequestChain[]) {
     return async (req: NextRequest, res: NextResponse) => {
+      /**
+       * Recursive middleware executor
+       * @param index - Current middleware index
+       * @param previousData - Data passed from previous middleware
+       * @returns Promise resolving to a NextResponse
+       */
       const execute = async (
         index: number,
         prev?: any
